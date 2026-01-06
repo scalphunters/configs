e55dd2f4052c9c3a5c90d4dba80f758e679435e1
SK Park <scalphunter@gmail.com>
2026-01-06 23:28:23 +0000

    ADD: git fugitive

--- vim_config/install.sh
+++ vim_config/install.sh
@@ -9,5 +9,9 @@ vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 # NERDCommenter
 git clone https://github.com/preservim/nerdcommenter.git ~/.vim/pack/vendor/start/nerdcommenter
 
-
-
+# Git fugitive
+#
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/fugitive.git
+vim -u NONE -c "helptags fugitive/doc" -c q
