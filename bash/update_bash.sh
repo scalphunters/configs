72c4bdf91290cc2b04616236e53bbae333ad2135
SK Park <scalphunter@gmail.com>
2026-07-05 23:10:03 +0000

    .

--- bash/update_bash.sh
+++ bash/update_bash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "alias ls='eza -alh --git --icons'" >> $HOME/.bashrc
 echo "alias lse='eza -alh --git --icons'" >> $HOME/.bashrc
-
+echo "alias ll='eza -alh --git --icons'" >> $HOME/.bashrc
+echo "alias lt='eza -alh --git --tree --icons'" >> $HOME/.bashrc
