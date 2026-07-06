7cdb5f45c90756fbe5052bce93cd9ec4774079c9
SK Park <scalphunter@gmail.com>
2026-07-05 20:39:38 -0400

    .

--- zsh/update_zsh.sh
+++ zsh/update_zsh.sh
@@ -2,8 +2,8 @@
 
 RCFILENAME=$HOME/.zshrc
 
-echo "alias la='eza'" >> $RCFILENAME
-echo "alias ls='eza -alh --git --icons'" >> $RCFILENAME
+echo "alias ls='eza'" >> $RCFILENAME
+echo "alias la='eza -alh --git --icons'" >> $RCFILENAME
 echo "alias lse='eza -alh --git --icons'" >> $RCFILENAME 
 echo "alias ll='eza -alh --git --icons'" >> $RCFILENAME 
 echo "alias lt='eza -alh --git --tree --icons'" >> $RCFILENAME 
